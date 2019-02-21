f507a45b13bb530d50cf9574cb0d1f8a35afc14c
Matt Ondo <matt.ondo@intelligentdemand.com>
2019-02-21 12:48:46 -0700

    removes malfunctioning plugin

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,7 +9,6 @@
       <p>My name is Matt. I am located in Denver, CO where I spend as much time as possible out in the Rocky Mountains. In the winter you can bet your butt I'm skiing. In the summer you'll find me mountain biking, camping, hiking, and/or enjoying a classic park day.</p>
       <p>Yes, I have a job–a pretty great one! My current post is Senior Front-end Developer at Intelligent Demand. I've been with Intelligent Demand since February, 2016. As a Senior Front-end Developer, I play a critical role in the Front-End Development practice at ID and own the conversion of original visual design concepts to bulletproof front-end implementations. I have experience in building, testing and deploying multi-touch, multi-channel marketing campaigns in marketing automation platform software. Additionally, I own the buildout, testing and deployment of interactive content and websites using WordPress and other CMS platforms.</p>
     </article>
-    <InstaFeed/>
   </Layout>
 </template>
 
@@ -17,11 +16,5 @@
 </style>
 
 <script>
-import InstaFeed from "@/components/Instafeed";
-
-export default {
-  components: {
-    InstaFeed
-  }
-};
+export default {};
 </script>
