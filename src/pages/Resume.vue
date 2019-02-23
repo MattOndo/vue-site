194f7bbef30bdcdbaba53e02eb3e63f27ee61e7c
Matt Ondo <yours.truly@mattondo.com>
2019-02-22 23:24:35 -0700

    fix: meta

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -12,7 +12,13 @@ export default {
   name: 'Resume',
   metaInfo: {
     title: 'Resume',
-    meta: [{ name: 'description', content: "Matt Ondo's Resume" }]
+    meta: [
+      {
+        name: 'description',
+        content:
+          "Welcome to Matt Ondo's Resume. Matt is a front-end developer and modern marketer. Here you will find Matt's portfolio, recent work, and contact information you will learn about Matt's recent adventures."
+      }
+    ]
   }
 }
 </script>
