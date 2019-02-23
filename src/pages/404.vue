e8ea056933c6ed8dd20e81b76a168dcc3af6bc07
Matt Ondo <yours.truly@mattondo.com>
2019-02-22 23:14:07 -0700

    adds 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <Layout>
+    <div class="mb6">
+      <h1 class="tc">404 😳</h1>
+      <p class="tc">Well this is embarrassing</p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: 'ContactMatt',
+  metaInfo: {
+    title: '404',
+    meta: [
+      {
+        name: 'description',
+        content: '404 Not Found'
+      }
+    ]
+  }
+}
+</script>
+
+<style scoped>
+</style>
