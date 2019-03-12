9bd11c40a34022d1ebeca6f36eb0c8f055789e48
Matt Ondo <matt.ondo@intelligentdemand.com>
2019-03-12 14:20:03 -0400

    creates partials

---
+++ src/partials/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="mw7 pa3 center tc pv4 mt4">
+    <div class="f7">
+      <a
+        class="black no-underline hover-blue"
+        href="https://github.com/MattOndo"
+        target="_blank"
+      >Github</a> |
+      <a
+        class="black no-underline hover-blue"
+        href="https://www.instagram.com/matt_ondo/"
+        target="_blank"
+      >Instagram</a> |
+      <a
+        class="black no-underline hover-blue"
+        href="https://www.linkedin.com/in/mattondo/"
+        target="_blank"
+      >LinkedIn</a>
+    </div>
+    <div class="flex items-center justify-center f7 mt3 w-100">
+      <Icon size="sm"/>
+      <span>&nbsp;&nbsp;is powered by&nbsp;&nbsp;</span>
+      <a href="https://gridsome.org/" target="_blank">
+        <g-image src="../assets/gridsome.png" class="dib" style="width:1.6rem;"/>
+      </a>
+    </div>
+    <p class="f7 mt3 mb0">&copy; {{year}} Matt Ondo</p>
+  </footer>
+</template>
+
+<script>
+import Icon from '../components/Icon'
+
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  },
+  components: {
+    Icon: Icon
+  }
+}
+</script>
---
+++ src/partials/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="mw7 pa3 center">
+    <DarkMode/>
+    <div class="vh-50 flex flex-column flex-row-ns items-center justify-center justify-end-ns">
+      <nav class="order-1 order-0-ns tc">
+        <g-link
+          class="pa2 no-underline f5 f4-l fw3 ttl dark-gray black-hover dib"
+          :to="{ name: 'home' }"
+        >home</g-link>
+        <g-link
+          class="pa2 no-underline f5 f4-l fw3 ttl dark-gray black-hover dib blog"
+          :to="{ name: 'blog' }"
+        >blog</g-link>
+        <g-link
+          class="pa2 no-underline f5 f4-l fw3 ttl dark-gray black-hover dib sandbox"
+          :to="{ name: 'sandbox' }"
+        >sandbox</g-link>
+        <g-link
+          class="pa2 no-underline f5 f4-l fw3 ttl dark-gray black-hover dib"
+          :to="{ name: 'contact' }"
+        >contact</g-link>
+      </nav>
+      <div class="order-0 order-1-ns db dib-ns">
+        <g-link class="pa2 cursor fw7" :to="{ name: 'home' }">
+          <Icon size="md"/>
+        </g-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import Icon from '../components/Icon'
+import DarkMode from '../components/DarkMode'
+
+export default {
+  name: 'Header',
+  components: {
+    Icon: Icon,
+    DarkMode: DarkMode
+  }
+}
+</script>
