7c1f285df134d5ebf8f9b120ea9f303acf356884
Matt Ondo <yours.truly@mattondo.com>
2019-04-08 21:21:57 -0600

    feat: adds projects

---
+++ src/components/MobileScreen.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mobile-wrap w-100">
+    <div class="mobile-screen bg-dark-gray" style="border-radius: 1.5rem;">
+      <img :src="src" class="ba bw2 b--dark-gray" style="border-radius: 1.5rem;">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MobileScreen',
+  props: ['src']
+}
+</script>
+
+<style scoped>
+</style>
