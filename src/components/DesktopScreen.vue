e85531d4c71f346e71a3daa98b36694517a9ff8f
Matt Ondo <yours.truly@mattondo.com>
2019-04-08 21:21:17 -0600

    feat: adds portfolio

---
+++ src/components/DesktopScreen.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="desktop-wrap w-100 bg-dark-gray ba bw1 b--dark-gray br--top br3">
+    <div class="desktop-topbar h1 flex flex-row items-center">
+      <span
+        class="dib bg-light-gray ba bw1 b--light-gray br br-100 mh1"
+        style="height:.5rem;width:.5rem;"
+      ></span>
+      <span
+        class="dib bg-light-gray ba bw1 b--light-gray br br-100 mh1"
+        style="height:.5rem;width:.5rem;"
+      ></span>
+      <span
+        class="dib bg-light-gray ba bw1 b--light-gray br br-100 mh1"
+        style="height:.5rem;width:.5rem;"
+      ></span>
+    </div>
+    <div class="desktop-screen">
+      <img v-if="type == 'image'" :src="src" alt>
+      <div v-else-if="type == 'video'" class>
+        <video class="w-100" controls>
+          <source :src="src" type="video/mp4">Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DesktopScreen',
+  props: ['src', 'type']
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/portfolio/Index.vue
@@ -0,0 +1,100 @@
+<template>
+  <Layout>
+    <h1>Matt's Portfolio</h1>
+    <div class="grid">
+      <a href="/portfolio/interactive-email" class="tc link">
+        <DesktopScreen type="image" src="/portfolio-assets/id_2017_interactive_holiday_thumb.png"/>
+        <h4>Interactive Invite Email</h4>
+      </a>
+
+      <a href="#" class="tc link black">
+        <DesktopScreen type="image" src="/portfolio-assets/intelligentdemand_landing.png"/>
+        <h4>Intelligent Demand Website</h4>
+      </a>
+
+      <a href="#" class="tc link black">
+        <DesktopScreen type="image" src/>
+        <h4>Interactive Infographic</h4>
+      </a>
+
+      <a href="#" class="tc link black">
+        <DesktopScreen type="image" src/>
+        <h4>Service Source Website</h4>
+      </a>
+
+      <a href="#" class="tc link black">
+        <DesktopScreen type="image" src/>
+        <h4>Service Source Japanese Website</h4>
+      </a>
+
+      <a href="#" class="tc link black">
+        <DesktopScreen type="image" src/>
+        <h4>Interactive Assessment</h4>
+      </a>
+
+      <a href="#" class="tc link black">
+        <DesktopScreen type="image" src/>
+        <h4>Marketo Template Suite</h4>
+      </a>
+
+      <a href="#" class="tc link black">
+        <DesktopScreen type="image" src="/portfolio-assets/id_2018_holiday_lp.png"/>
+        <h4>Interactive Story Book</h4>
+      </a>
+
+      <a href="/portfolio/east-end-salon" class="tc link black">
+        <DesktopScreen type="image" src="/portfolio-assets/east_end_homepage.png"/>
+        <h4>East End Salon</h4>
+      </a>
+    </div>
+  </Layout>
+</template>
+
+<style scoped>
+@media screen and (min-width: 1024px) {
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: '. . .' '. . .';
+    grid-gap: 1rem;
+  }
+}
+</style>
+
+<script>
+import DesktopScreen from '../../components/DesktopScreen'
+
+export default {
+  metaInfo() {
+    let currentUrl = `https://mattondo.io${this.$route.fullPath}`
+    return {
+      title: 'Portfolio',
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content:
+            "Welcome to Matt Ondo's Portfolio. Matt is a front-end developer and modern marketer. Here you will find Matt's portfolio, recent work, and contact information you will learn about Matt's recent adventures."
+        }
+      ],
+      script: [
+        {
+          key: 'structuredData',
+          type: 'application/ld+json',
+          innerHTML: `{
+          '@context': 'http://schema.org/',
+          '@type': 'WebSite',
+          name: 'Portfolio - Matt Ondo',
+          url: '${currentUrl}'
+        }`
+        }
+      ],
+      __dangerouslyDisableSanitizers: ['script']
+    }
+  },
+  components: {
+    DesktopScreen: DesktopScreen
+  }
+}
+</script>
