16c0c96c1c8c8e06365a81c012e58a7a297506de
Matt Ondo <themattondo@gmail.com>
2019-04-10 10:06:00 -0600

    updates device componants to allow for full page screenshots

--- src/components/DesktopScreen.vue
+++ src/components/DesktopScreen.vue
@@ -1,22 +1,18 @@
 <template>
-  <div class="desktop-wrap w-100 bg-dark-gray ba bw1 b--dark-gray br--top br3">
-    <div class="desktop-topbar h1 flex flex-row items-center">
-      <span
-        class="dib bg-light-gray ba bw1 b--light-gray br br-100 ml1"
-        style="height:12px;width:12px;"
-      ></span>
-      <span
-        class="dib bg-light-gray ba bw1 b--light-gray br br-100 mh1"
-        style="height:12px;width:12px;"
-      ></span>
-      <span
-        class="dib bg-light-gray ba bw1 b--light-gray br br-100"
-        style="height:12px;width:12px;"
-      ></span>
+  <div class="device">
+    <div class="device-header flex flex-row items-center">
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
-    <div class="desktop-screen">
-      <img v-if="type == 'image'" :src="src" alt>
-      <div v-else-if="type == 'video'" class>
+    <div class="device-screen">
+      <div
+        class="device-screen--image"
+        v-bind:class="doScroll"
+        v-if="type == 'image'"
+        :style="{'background-image': 'url(' + src + ')'}"
+      ></div>
+      <div class="device-screen--video" v-else-if="type == 'video'">
         <video class="w-100" controls>
           <source :src="src" type="video/mp4">Your browser does not support the video tag.
         </video>
@@ -28,9 +24,91 @@
 <script>
 export default {
   name: 'DesktopScreen',
-  props: ['src', 'type']
+  props: ['src', 'type', 'scroll'],
+  computed: {
+    doScroll: function() {
+      return this.scroll ? 'scroll' : ''
+    }
+  }
 }
 </script>
 
 <style scoped>
+@-webkit-keyframes device_scroll {
+  0% {
+    background-position: top center;
+  }
+  10% {
+    background-position: top center;
+  }
+  50% {
+    background-position: center center;
+  }
+  90% {
+    background-position: bottom center;
+  }
+  100% {
+    background-position: bottom center;
+  }
+}
+@keyframes device_scroll {
+  0% {
+    background-position: top center;
+  }
+  10% {
+    background-position: top center;
+  }
+  50% {
+    background-position: center center;
+  }
+  90% {
+    background-position: bottom center;
+  }
+  100% {
+    background-position: bottom center;
+  }
+}
+.device {
+  background: #333;
+  width: 100%;
+  border: 5px solid #333;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+.device-header {
+  height: 1rem;
+  padding: 0 0 5px 5px;
+}
+.device-header span {
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  background: #d0d0d0;
+  margin-right: 5px;
+}
+.darkMode .device {
+  background: #d0d0d0;
+  border: 5px solid #d0d0d0;
+}
+.darkMode .device-header span {
+  background: #333;
+}
+.device-screen--image {
+  padding-bottom: 56.25%;
+  background-size: cover;
+  background-position: top center;
+}
+.device-screen--image.scroll {
+  background-size: 100%;
+  -webkit-animation-name: device_scroll;
+  animation-name: device_scroll;
+  -webkit-animation-duration: 20s;
+  animation-duration: 20s;
+  -webkit-animation-timing-function: linear;
+  animation-timing-function: linear;
+  -webkit-animation-iteration-count: infinite;
+  animation-iteration-count: infinite;
+  -webkit-animation-delay: 1500ms;
+  animation-delay: 1500ms;
+}
 </style>
--- src/components/MobileScreen.vue
+++ src/components/MobileScreen.vue
@@ -1,17 +1,90 @@
 <template>
-  <div class="mobile-wrap w-100">
-    <div class="mobile-screen bg-dark-gray" style="border-radius: 1rem;">
-      <img :src="src" class="ba bw2 b--dark-gray" style="border-radius: 1rem;">
-    </div>
+  <div class="device">
+    <div class="device-header"></div>
+    <div
+      class="device-screen"
+      v-bind:class="doScroll"
+      :style="{'background-image': 'url(' + src + ')'}"
+    ></div>
+    <div class="device-footer"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MobileScreen',
-  props: ['src']
+  props: ['src', 'scroll'],
+  computed: {
+    doScroll: function() {
+      return this.scroll ? 'scroll' : ''
+    }
+  }
 }
 </script>
 
 <style scoped>
+@-webkit-keyframes device_scroll {
+  0% {
+    background-position: top center;
+  }
+  10% {
+    background-position: top center;
+  }
+  50% {
+    background-position: center center;
+  }
+  90% {
+    background-position: bottom center;
+  }
+  100% {
+    background-position: bottom center;
+  }
+}
+@keyframes device_scroll {
+  0% {
+    background-position: top center;
+  }
+  10% {
+    background-position: top center;
+  }
+  50% {
+    background-position: center center;
+  }
+  90% {
+    background-position: bottom center;
+  }
+  100% {
+    background-position: bottom center;
+  }
+}
+.device {
+  background: #333;
+  width: 100%;
+  border: 5px solid #333;
+  border-radius: 25px;
+}
+.darkMode .device {
+  background: #d0d0d0;
+  border: 5px solid #d0d0d0;
+}
+.device-header,
+.device-footer {
+  height: 30px;
+}
+.device-screen {
+  padding-bottom: calc(2960 / 1440 * 100%);
+  background-size: 100%;
+}
+.device-screen.scroll {
+  -webkit-animation-name: device_scroll;
+  animation-name: device_scroll;
+  -webkit-animation-duration: 10s;
+  animation-duration: 10s;
+  -webkit-animation-timing-function: linear;
+  animation-timing-function: linear;
+  -webkit-animation-iteration-count: infinite;
+  animation-iteration-count: infinite;
+  -webkit-animation-delay: 500ms;
+  animation-delay: 500ms;
+}
 </style>
